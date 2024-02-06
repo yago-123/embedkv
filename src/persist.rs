85e8876328a0c4ca768759a2edc7fd4af85d037a
yago-123 <yfernandezgou@gmail.com>
2024-02-06 20:17:58 +0200

    persist: implement first iteration of update_kv and introduce Error enum

--- src/persist.rs
+++ src/persist.rs
@@ -1,10 +1,18 @@
 use std::collections::BTreeMap;
+use std::io;
 use std::io::{Error, ErrorKind, Seek, SeekFrom, Write};
 use std::os::unix::fs::FileExt;
 use crate::fileheader::FileHeader;
 use crate::freelist::FreeList;
 use crate::slot::Slot;
 use serde::{Serialize, Deserialize};
+use crate::persist::KVError::KeyDoesNotExist;
+
+#[derive(Debug)]
+enum KVError {
+    KeyDoesNotExist,
+    IOError(std::io::Error),
+}
 
 pub struct Persister<K> {
     freelist: FreeList,  
@@ -14,9 +22,10 @@ pub struct Persister<K> {
 }
 
 impl<K> Persister<K> where K: Ord {
-    pub fn new(datastore: String, storage_limit: usize) -> Result<Self, std::io::Error> {
+    pub fn new(datastore: String, storage_limit: usize) -> Result<Self, KVError> {
         FileHeader::new(Some(datastore))
             .map(|fh| Self { freelist: FreeList::new(), header: fh, index: BTreeMap::new(), last_cursor: 0 })
+            .map_err(|io_error| KVError::IOError(io_error))
     }
 
     pub fn insert_kv<'a>(&mut self, key: K, value: &Vec<u8>) -> Result<usize, std::io::Error>
@@ -34,7 +43,7 @@ impl<K> Persister<K> where K: Ord {
             }
         }
 
-        if let Err(error) = self.insert_value(&value, cursor) {
+        if let Err(error) = self.persist_value(&value, cursor) {
             // make sure to free the memory to prevent leaks
             if cursor == self.last_cursor - value.len() {
                 self.last_cursor = cursor - value.len()
@@ -68,19 +77,73 @@ impl<K> Persister<K> where K: Ord {
         }
     }
 
-    pub fn update_kv(&mut self, key: K) {
+    pub fn update_kv(&mut self, key: &K, value: &Vec<u8>) -> Result<(), KVError> {
+        let mut slot = Slot{space: 0, cursor: 0};
+        match self.index.get(key) {
+            Some(val) => {
+                slot = val.clone();
+            },
+            None => return Err(KeyDoesNotExist),
+        }
 
+        // free previous data and claim more space
+        if value.len() > slot.space {
+            self.freelist.insert_free_space(slot.cursor, slot.space);
+            match self.freelist.retrieve_free_space(slot.space) {
+                Some(val) => {
+                    slot.cursor = val;
+                },
+                None => {
+                    slot.cursor = self.last_cursor;
+                    self.last_cursor = self.last_cursor + value.len();
+                },
+            }
+        }
+
+        // downsize the leftover space if the space is smaller
+        if value.len() < slot.space {
+            self.freelist.insert_free_space(slot.cursor+value.len(), slot.space - value.len());
+        }
+
+        // update slot space required
+        slot.space = value.len();
+
+        // persist the value
+        self.persist_value(value, slot.cursor);
+
+        // todo(): serialize the new key data
+
+        // update the index
+        self.index.insert(key, Slot{cursor: slot.cursor, space: slot.space});
+
+        return Ok(())
     }
 
-    pub fn delete_kv(&mut self, key: K) {
+    pub fn delete_kv(&mut self, key: &K) -> Result<(), KVError> {
+        // check if key exists and insert freed space
+        match self.index.get(key) {
+            Some(val) => self.freelist.insert_free_space(val.cursor, val.space),
+            None => return Err(KeyDoesNotExist),
+        }
 
+        // todo(): remove serialized key from file
+
+        // remove key from index
+        match self.index.remove(key) {
+            Some(_) => Ok(()),
+            None => Err(KeyDoesNotExist), // should never happen
+        }
     }
 
-    fn insert_value(&mut self, data: &Vec<u8>, cursor: usize) -> Result<(), std::io::Error> {
+    fn persist_value(&mut self, data: &Vec<u8>, cursor: usize) -> Result<(), std::io::Error> {
         self.header.db_file.seek(SeekFrom::Start(cursor as u64))?;
         self.header.db_file.write_all(data.as_ref())?;
 
         Ok(())
+    }
+
+    fn persist_key(&mut self) -> Result<(), KVError> {
+        return Ok(());
     }
 
     fn retrieve_value(&mut self, cursor: usize, space: usize) -> Result<Vec<u8>, std::io::Error> {
@@ -92,5 +155,31 @@ impl<K> Persister<K> where K: Ord {
         self.header.db_file.read_at(&mut buffer.as_mut_slice(), cursor as u64)?;
 
         return Ok(buffer)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_insert_kv() {
+        assert_eq!(1, 2)
+    }
+
+    #[test]
+    fn test_get_value() {
+        assert_eq!(1, 2)
+    }
+
+    #[test]
+    fn test_update_value() {
+        assert_eq!(1, 2)
+    }
+
+    #[test]
+    fn delete_kv() {
+        assert_eq!(1, 2)
     }
 }
